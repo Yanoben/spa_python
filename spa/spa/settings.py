4028bd5997985e67d82fd44c0a9c4a58792aa3d8
Ghost <yasinabdurakhmanov@gmail.com>
2022-04-15 06:24:49 +0300

    changes

--- spa/spa/settings.py
+++ spa/spa/settings.py
@@ -1,17 +1,15 @@
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a$s=)$0o$acxyv*-(m1nf+4jn&3g&u&@wrsdp(+g*407^iujsg'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -68,7 +66,7 @@ WSGI_APPLICATION = 'spa.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
